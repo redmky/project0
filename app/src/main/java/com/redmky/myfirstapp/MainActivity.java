8a10faf806b50db7be555bc3fb34aee690520637
redmky <migtinoco@gmail.com>
2015-09-22 13:24:53 -0700

    modifications according to the review

--- app/src/main/java/com/redmky/myfirstapp/MainActivity.java
+++ app/src/main/java/com/redmky/myfirstapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.redmky.myfirstapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,10 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void showToast(View view) {
+    public void displayToast(View view) {
 
-        Toast.makeText(getApplicationContext(),
-                "This button will launch my capstone app!", Toast.LENGTH_LONG).show();
+        Button button = (Button) view;
+
+        String buttonText = (String) button.getText();
+
+        Context context = getApplicationContext();
+        // open_app would add "Opens the app "
+        CharSequence text = getString(R.string.open_app)
+                + buttonText;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     @Override
